83bf4b1fd3e345ad84c38e84184bb1e284add9df
chouhanbanti2544 <banticonstruction@gmail.com>
2022-09-25 23:35:43 +0530

    one

---
+++ Assignment2.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Assignment 2
+
+# In[ ]:
+
+
+Ans1. True and False are the two value of the boolean data type.
+
+
+# Ans2. There are three different type of Boolean operators.
+#       1) And 2)or 3) Not
+
+# Ans3.
+#      
+
+# In[9]:
+
+
+i = 5
+
+if i == 5 and i<=10:
+    print("do something")
+
+
+# In[10]:
+
+
+i = 5
+
+if i == 5 or i<=10:
+    print("do something")
+
+
+# Ans 4.
+
+# In[ ]:
+
+
+a) (5>4) and (3==5)                          Ans - False
+b) not(5>4)                                  Ans - False
+c) not ((5>4) and (3==5))                    Ans - True
+d) (True and True) and (True == False)       Ans - False
+e) (not False ) or (not True)                Ans - True
+
+
+# Ans5.
+
+# In[ ]:
+
+
+Six comparison operator -
+Less than ( < )
+Less than or equal to ( <= )
+Greater than ( > )
+Greater than or equal to ( >= )
+Equal to ( == )
+Not equal to ( != )
+
+
+# Ans6.
+
+# In[ ]:
+
+
+The “=” is an assignment operator is used to assign the value on the right to the variable on the left. 
+The '==' operator checks whether the two given operands are equal or not. If so, it returns true. 
+Otherwise it returns false
+
+
+# In[18]:
+
+
+i = 5 #assignment operator
+
+if 5 == 5:         #equal to operator
+    print("yes")
+
+
+# Ans8. 
+
+# In[23]:
+
+
+spam = 1
+if 1==1:
+    print("Hello")
+
+
+# In[25]:
+
+
+spam = 2
+if 2==2:
+    print("Howdy")
+
+
+# In[26]:
+
+
+spam = 2
+if 2==1:
+    print("Howdy")
+else:
+    print("Greeting")
+
+
+# Ans9.
+
+# In[ ]:
+
+
+Menubar - Kernal - Restart
+
+
+# Ans10.
+
+# Break - The main difference between both the statements is that when break keyword comes, 
+# it terminates the execution of the current loop and passes the control over the next loop or main body
+# 
+# Continue - whereas when continue keyword is encountered, it skips the current iteration and executes the very next
+# iteration in the loop
+
+# Ans11. 
+
+# In[38]:
+
+
+for i in range(10):
+    print(i)
+
+
+# In[39]:
+
+
+for i in range(0,10):
+    print(i)
+
+
+# In[40]:
+
+
+for i in range(0,10,1):
+    print(i)
+
+
+# Ans12.
+
+# In[42]:
+
+
+for i in range(1,11):
+    print(i)
+
+
+# In[43]:
+
+
+a = 1
+b = 10
+while a<=b:
+    print(a)
+    a = a+1
+
+
+# Ans13.
+
+# In[49]:
+
+
+def bacon():
+    spam = bacon
+    
+
+
+# In[50]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
